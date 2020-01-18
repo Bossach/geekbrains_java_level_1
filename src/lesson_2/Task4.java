ca3e99a9b82e0ce877f08d5f7d576e9397f1d0b9
bossach <boss52@inbox.ru>
2020-01-18 16:44:02 +0300

    lesson 2 task 4

---
+++ src/lesson_2/Task4.java
@@ -0,0 +1,28 @@
+package lesson_2;
+
+// 4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое), и с помощью цикла(-ов) заполнить его диагональные элементы единицами;
+
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		int squareArrSize = 8;
+		
+		int[][] squareArr = new int[ squareArrSize ][ squareArrSize ];
+		
+		for ( int i = 0 ; i < squareArr.length ; i++ )
+			System.out.println( Arrays.toString( squareArr [ i ]) );
+		System.out.println("------------------------");
+		
+		
+		for ( int i = 0 ; i < squareArr.length ; i++ ) {
+			 squareArr[ i ][ i ] = 1;
+			 squareArr[ i ][ squareArr.length - i - 1 ] = 1;
+		}
+		
+		for ( int i = 0 ; i < squareArr.length ; i++ )
+			System.out.println( Arrays.toString( squareArr[ i ] ) );		
+		
+	}
+}
