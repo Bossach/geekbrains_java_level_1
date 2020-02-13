1f1009a9d712a2d6228dd4674984642b17b4704c
bossach <boss52@inbox.ru>
2020-02-13 18:23:56 +0300

    lesson_8

---
+++ src/lesson_8/Main.java
@@ -0,0 +1,14 @@
+package lesson_8;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        
+        //Прошу меня простить
+        //Завал на работе
+        //Пашу и днём и ночью😀
+        
+    }
+    
+}
